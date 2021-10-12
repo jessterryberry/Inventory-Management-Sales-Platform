633ee86098e6f31a61a1ae5852ea5245aeb4978d
jessterryberry <jlterryb@gmail.com>
2021-10-12 13:24:09 -0400

    Finished Customer

--- js/crud.js
+++ js/crud.js
@@ -1,71 +1,167 @@
-function customerDelete(ctl) {
-    if (confirm("Are you sure you want to delete this customer?")) {
-        $(ctl).parents("tr").remove();
-    }
-}
-
-function customerBuildTableRow() {
-    var ret = "<tr>" +
-        "<td>" + $("#inputFirstName").val() + "</td>" +
-        "<td>" + $("#inputLastName").val() + "</td>" +
-        "<td class='hide-col'>" + $("#inputAddress").val() + "</td>" +        
-        "<td>" + $("#inputCity").val() + "</td>" +
-        "<td class='hide-col'>" + $("#inputPostalCode").val() + "</td>" +
-        "<td>" + $("#inputEmail").val() + "</td>" +
-        "<td>" + $("#inputPhone").val() + "</td>" +
-        "<td>" +
-        "<button class='btn btn-outline-secondary' onclick='customerDisplay(this);' type='button' data-bs-toggle='collapse' id='editToggle' href='#collapseEdit' aria-expanded='false' aria-controls='collapseEdit'>Edit</button>" +
-        "</td>" +
-        "<td>" + "<button type='button' class='btn btn-outline-secondary' onclick= 'customerDelete(this);' data-id='1'>Delete</button>" + "</td>" +
-        "</tr>"  
-        
-        return ret;
-    }
-
 //current customer being edited
 var editRow = null;
 
-function customerDisplay(ctl) {
+function customerDelete(ctl) {
+    let myStorage = window.localStorage;    
+    
+    if (confirm("Are you sure you want to delete this customer?")) {
+        myStorage.removeItem($("#inputCustNumber").val().toString());        
+    }
+    loadCustomerTable();
+}
+
+function customerDisplay(ctl, mode) {
     editRow = $(ctl).parents("tr");
     var cols = editRow.children("td");
+    var customerID = $(cols[0]).text();
+    let myStorage = window.localStorage;
+    var customerData = JSON.parse(myStorage.getItem(customerID.toString()));
+        
+        $("#inputCustNumber").val(customerData[0]);
+        $("#inputFirstName").val(customerData[1]);
+        $("#inputLastName").val(customerData[2]);
+        $("#inputAddress").val(customerData[3]);
+        $("#inputCity").val(customerData[4]);
+        $("#inputPostalCode").val(customerData[5]);
+        $("#inputEmail").val(customerData[6]);
+        $("#inputPhone").val(customerData[7]);
+    
+    const updateButton = document.getElementById('updateButton');
+    const deleteButton = document.getElementById('deleteButton'); 
 
-        $("#inputFirstName").val($(cols[0]).text());
-        $("#inputLastName").val($(cols[1]).text());
-        $("#inputAddress").val($(cols[2]).text());
-        $("#inputCity").val($(cols[3]).text());
-        $("#inputPostalCode").val($(cols[4]).text());
-        $("#inputEmail").val($(cols[5]).text());
-        $("#inputPhone").val($(cols[6]).text());
-    //Changing button to update
-    $("#updateButton").text("Update");
+    if (mode == 1) {
+        updateButton.disabled = false;
+        deleteButton.disabled = true;
+    }
+    else if (mode == 2){
+        deleteButton.disabled = false;
+        updateButton.disabled = true;
+    } 
+    else {
+        deleteButton.disabled = true;
+        updateButton.disabled = true;
+    }   
 }
 
 function customerUpdate() {
-    customerUpdateInTable();
-    
-    //clear the fields
-    formClear();  
+    let myStorage = window.localStorage;
+    var cols = editRow.children("td");
+    var customerID = $(cols[0]).text();
+    var customerUpdate = [customerID.toString(), $("#inputFirstName").val(), $("#inputLastName").val(), $("#inputAddress").val(), $("#inputCity").val(), $("#inputPostalCode").val(), $("#inputEmail").val(), $("#inputPhone").val()];
+
+    myStorage.setItem(customerID.toString(), JSON.stringify(customerUpdate));  
+
+    loadCustomerTable();
 }
 
-function customerUpdateInTable() {
-    //add changed customer to the table
-    $(editRow).after(customerBuildTableRow());
-
-    //remove old customer row
-    $(editRow).remove();
-
-    //clear the form
-    formClear();
+//Function to create a first set of "hardcoded" example customers (same ones we currently have, just create them here instead of hardcoding them into the table)
+//could add a Demo button to call this to create a set of customers to start with. really only needs to run one time
+function demoData(){
+	let myStorage = window.localStorage;
+    let customer1 = ['1', 'Joe', 'Smith', '12 Main St', 'Welland', 'L3C 6M1', 'jsmith@gmail.com', '905-346-7367'];
+    let customer2 = ['2', 'Sarah', 'Yates', '35 W Main St', 'NOTL', 'L2C 7S4', 'ssyates@gmail.com', '905-869-2543'];
+    let customer3 = ['3', 'Nathan', 'Jones', '72 Side Rd', 'Wainfleet', 'L0R 2A0', 'njones@gmail.com', '289-439-9632'];
+    let customer4 = ['4', 'Farrah', 'Parker', '10 Ontario St', 'Toronto', 'M1H 6L8', 'faparker@gmail.com', '416-834-9768'];
+	
+	//save them each to local storage
+	//myStorage.setItem only stores strings -> use JSON.stringify to save the array as strings and JSON.parse to put them back into an array when loading
+	myStorage.setItem('1', JSON.stringify(customer1));
+	myStorage.setItem('2', JSON.stringify(customer2));
+	myStorage.setItem('3', JSON.stringify(customer3));
+	myStorage.setItem('4', JSON.stringify(customer4));
+	
+	//save a total number of customers so we can loop later to load the data
+	//myStorage.setItem only stores strings -> saving the number as a string, can use parseInt() to convert it to a number when loading
+	myStorage.setItem('customerAmount', '4');
 }
 
-function customerUpdateDDL() {
-    if ($("#customers option:selected").text = "Joe Smith") {
-        $("#inputFirstName").val();
-        $("#inputLastName").val();
-        $("#inputAddress").val();
-        $("#inputCity").val();
-        $("#inputPostalCode").val();
-        $("#inputEmail").val($(cols[5]).text());
-        $("#inputPhone").val($(cols[6]).text());
-    }
+//Function to save/add a new customer
+function saveCustomer(){
+    let myStorage = window.localStorage;
+	//loading current total amount of customers
+	let customerAmount = parseInt(myStorage.getItem('customerAmount'));
+	
+	//increasing total by 1 since we are adding a customer
+	customerAmount++;
+	console.log(customerAmount);
+	//creating new customer string array from inputs
+	let newCustomer = [customerAmount.toString(), $("#inputFirstName").val(), $("#inputLastName").val(), $("#inputAddress").val(), $("#inputCity").val(), $("#inputPostalCode").val(), $("#inputEmail").val(), $("#inputPhone").val()];
+	console.log(newCustomer);
+	//saving the new customer to myStorage
+	myStorage.setItem(customerAmount.toString(), JSON.stringify(newCustomer))
+	
+	//saving the new total customer amount
+	myStorage.setItem('customerAmount', customerAmount.toString())
 }
+//slightly changed version of the customerBuildTableRow we had already
+function customerBuildTableRowLoad(id, first, last, address, city, post, email, phone) {
+    var ret = "<tr>" +
+        "<td class='hide-col'>" + id + "</td>" +
+        "<td>" + first + "</td>" +
+        "<td>" + last + "</td>" +
+        "<td class='hide-col'>" + address + "</td>" +        
+        "<td>" + city + "</td>" +
+        "<td class='hide-col'>" + post + "</td>" +
+        "<td>" + email + "</td>" +
+        "<td>" + phone + "</td>" +
+        "<td>" +
+        "<button class='btn btn-outline-secondary' onclick='customerDisplay(this, 3);' style='color:black;' type='button' data-bs-toggle='collapse' id='editToggle' href='#collapseEdit' aria-expanded='false' aria-controls='collapseEdit'>Details</button>" +
+        "</td>" +
+        "<td>" +
+        "<button class='btn btn-outline-secondary' onclick='customerDisplay(this, 1);' style='color:black;' type='button' data-bs-toggle='collapse' id='editToggle' href='#collapseEdit' aria-expanded='false' aria-controls='collapseEdit'>Edit</button>" +
+        "</td>" +
+        "<td>" + "<button class='btn btn-outline-secondary' onclick='customerDisplay(this, 2);' style='color:black;' type='button' data-bs-toggle='collapse' id='editToggle' href='#collapseEdit' aria-expanded='false' aria-controls='collapseEdit'>Delete</button>" + "</td>" +
+        "</tr>"  
+        
+	return ret;
+}
+//Function to load all of our customers into a table for CustomerAll page
+function loadCustomerTable(){
+	//checking total amount of customers
+	let myStorage = window.localStorage;
+    let customerAmount = parseInt(myStorage.getItem('customerAmount'));
+    let custTable = "";
+	
+	//looping through 1 to total amount of customers
+	for (let i = 1; i <= customerAmount; i++){
+		//loading the customer data and parsing the string back into a string array if there is data stored
+		if (myStorage.getItem(i.toString()) !== null){
+			let c = JSON.parse(myStorage.getItem(i.toString()));
+
+			//building a table row with the customer's information
+			//c[0] to c[6] are the elements in the array -> first name, last name, address, ...
+			custTable = custTable + customerBuildTableRowLoad(c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7]);
+		}
+        
+	}
+    document.getElementById("customerTable").innerHTML = custTable;
+}
+
+function filterCustomerTable(){
+	//checking total amount of customers
+	let myStorage = window.localStorage;
+    let customerAmount = parseInt(myStorage.getItem('customerAmount'));
+    let custTable = "";
+	
+	//looping through 1 to total amount of customers
+	for (let i = 1; i <= customerAmount; i++){
+		//loading the customer data and parsing the string back into a string array if there is data stored
+		if (myStorage.getItem(i.toString()) !== null){
+			let c = JSON.parse(myStorage.getItem(i.toString()));
+
+			//building a table row with the customer's information
+			//c[0] to c[6] are the elements in the array -> first name, last name, address, ...
+            if ($('#filterFirstName').val() !== "" && c[1].toUpperCase().includes($('#filterFirstName').val().toUpperCase())){
+                custTable = custTable + customerBuildTableRowLoad(c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7]);                
+            }
+			else if ($('#filterLastName').val() !== "" && c[2].toUpperCase().includes($('#filterLastName').val().toUpperCase())){
+                custTable = custTable + customerBuildTableRowLoad(c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7]);
+            }
+            else if ($('#filterCity').val() !== "" && c[4].toUpperCase().includes($('#filterCity').val().toUpperCase())){
+                custTable = custTable + customerBuildTableRowLoad(c[0],c[1],c[2],c[3],c[4],c[5],c[6],c[7]);
+            }
+		}        
+	}
+    document.getElementById("customerTable").innerHTML = custTable;
+}
+
