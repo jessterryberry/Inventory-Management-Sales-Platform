3ac0d876a2720a77ae271ebf97303130902e4d39
DTtruong3 <dtruong3@ncstudents.niagaracollege.ca>
2021-12-12 21:36:18 -0500

    Updated Seed data

--- js/crud_inventory.js
+++ js/crud_inventory.js
@@ -59,15 +59,15 @@ function inventoryUpdate() {
 
 function demoInvData(){
 	let myStorage2 = window.localStorage;
-    let inventory1 = ['inventoryID:1', '2 stroke oil (Blue)', 'Blue - Oil for a two stroke engine.', 'Castrol', '6 oz', '24', '4.95'];
-    let inventory2 = ['inventoryID:2', '2 stroke oil (Red)', 'Red - Oil for a two stroke engine.', 'Penzoil', '6 oz', '19', '5.55'];
-    let inventory3 = ['inventoryID:3', '4 stroke oil', 'Oil for a four stroke engine.', 'Castrol', '24 oz', '36', '10.99'];
-    let inventory4 = ['inventoryID:4', '50:1 Mixed Gas', 'Pre-mixed gas for two stroke engines requiring a 50:1 gas to oil ratio.', 'Castrol', '1 L', '28', '12.99'];
+    let inventory1 = ['inventoryID:1', '2 stroke oil (Blue), 6 oz', 'Blue - Oil for a two stroke engine.', 'Castrol', '6 oz', '24', '4.95'];
+    let inventory2 = ['inventoryID:2', '2 stroke oil (Red), 6 oz', 'Red - Oil for a two stroke engine.', 'Penzoil', '6 oz', '19', '5.55'];
+    let inventory3 = ['inventoryID:3', '4 stroke oil, 24 oz', 'Oil for a four stroke engine.', 'Castrol', '24 oz', '36', '10.99'];
+    let inventory4 = ['inventoryID:4', '50:1 Mixed Gas, 1 L', 'Pre-mixed gas for two stroke engines requiring a 50:1 gas to oil ratio.', 'Castrol', '1 L', '28', '12.99'];
     let inventory5 = ['inventoryID:5', 'Replacement Blade', 'For mower.', 'Honda', '', '7', '19.99'];
     let inventory6 = ['inventoryID:6', 'Green Cutter', 'Gasoline powered lawn mower.', 'John Deere', '', '5', '59.99'];
     let inventory7 = ['inventoryID:7', 'Kevlar Replacement Belt', 'ORB-H-1067 Murray 585416 or Ariens 07200021 Kevlar Replacement Belt.', 'D&D PowerDrive', '1/2" x 38"', '11', '18.40'];
     let inventory8 = ['inventoryID:8', 'OEM 868 Engine Spark Plug', 'Genuine, OEM Champion Replacement Part, part number 868.', 'Champion', '2.54 x 2.54 x 10.16 cm', '23', '14.16'];
-    let inventory9 = ['inventoryID:9', '30W Engine Oil', 'High quality SAE30 oil, specially formulated for higher operating temperatures of air cooled engines.', 'Briggs & Stratton', '1 L', '18', '42.99'];
+    let inventory9 = ['inventoryID:9', '30W Engine Oil, 1 L', 'High quality SAE30 oil, specially formulated for higher operating temperatures of air cooled engines.', 'Briggs & Stratton', '1 L', '18', '42.99'];
     
 	
     myStorage2.setItem('inventoryID:1', JSON.stringify(inventory1));
